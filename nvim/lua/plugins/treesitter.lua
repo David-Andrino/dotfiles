e3c1635074f0ae7ce6ddf2b982dd37e2031cc7a7
David-Andrino <davidandrino2002@gmail.com>
2023-12-24 17:29:34 +0100

    Fix nvim treesitter typo

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate"
+    build = ":TSUpdate",
     opts = {
         ensure_installed = { "c", "cpp", "make", "cmake", "rust", "python", "lua" },
         highlight = { enable = true },
