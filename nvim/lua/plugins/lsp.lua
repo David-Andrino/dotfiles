7c53c1dbf4bd89ca325f1d136cc50bc68e057a11
David-Andrino <davidandrino2002@gmail.com>
2023-12-25 23:10:32 +0100

    Fuse lsp and mason

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,13 +1,24 @@
 return {
-    "neovim/nvim-lspconfig",
-    keys = {
-        { 'K', vim.lsp.buf.hover },
-        { 'gd', vim.lsp.buf.definition },
-        { '<leader>la', vim.lsp.buf.code_action },
-        { '<leader>lr', vim.lsp.buf.rename }
+    {
+        "williamboman/mason.nvim",
+        opts = {}
+    }, {
+        "williamboman/mason-lspconfig",
+        opts = {
+            ensure_installed = { "lua_ls" }
+        },
     },
-    config = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup()
-    end
+    {
+        "neovim/nvim-lspconfig",
+        keys = {
+            { 'K', vim.lsp.buf.hover },
+            { 'gd', vim.lsp.buf.definition },
+            { '<leader>la', vim.lsp.buf.code_action, desc="Code actions" },
+            { '<leader>lr', vim.lsp.buf.rename, desc="Document symbols" }
+        },
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+        end
+    }
 }
