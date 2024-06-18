b93e3e92f596c14b63f07f8c553940878323781d
David Andrino <davidandrino2002@gmail.com>
2024-06-18 21:32:55 +0200

    Create stow folder structure

--- nvim/tmp/src/main.rs
+++
@@ -1,6 +0,0 @@
-use std::{rc::Rc, error::Error};
-
-fn main() -> Result<Rc<i32>, Box<dyn Error>> {
-    let mut a: Vec<i32> = vec![1, 2, 3];
-    Ok(Rc::new(a.pop().unwrap()))
-}
--- nvim/tmp/target/debug/deps/tmp-9d39ebb54f830342.d
+++
@@ -1,9 +0,0 @@
-/home/david/.dotfiles/nvim/tmp/target/debug/deps/libtmp-9d39ebb54f830342.rmeta: src/main.rs Cargo.toml
-
-/home/david/.dotfiles/nvim/tmp/target/debug/deps/tmp-9d39ebb54f830342.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
