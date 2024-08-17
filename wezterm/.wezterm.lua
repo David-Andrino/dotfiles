faa121afa612a3c5642a780de79fb697adfb7f55
David Andrino <davidandrino2002@gmail.com>
2024-08-17 15:48:49 +0200

    Add right click copy and paste to wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,6 +9,22 @@ config.font = wezterm.font 'Cascadia Code NF'
 config.font_size = 18
 
 config.color_scheme = 'Catppuccin Mocha'
+
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
