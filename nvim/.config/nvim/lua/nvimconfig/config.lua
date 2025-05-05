10307edc7ffc82655eccfbae8ae4ff752852f87f
David Andrino <davidandrino2002@gmail.com>
2025-05-05 12:57:45 +0200

    Create new neovim configuration

---
+++ nvim/.config/nvim/lua/nvimconfig/config.lua
@@ -0,0 +1,15 @@
+local o = vim.opt
+
+o.nu             = true
+o.relativenumber = false
+o.scrolloff      = 8
+
+o.expandtab   = true
+o.tabstop     = 4
+o.softtabstop = 4
+o.shiftwidth  = 4
+o.signcolumn  = "yes"
+
+o.wrap          = true
+o.incsearch     = true
+o.termguicolors = true
