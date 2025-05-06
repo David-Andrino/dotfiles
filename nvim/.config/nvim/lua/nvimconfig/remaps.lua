34fa41e7db221b888e535bfa6d7a744841453345
David Andrino <davidandrino2002@gmail.com>
2025-05-06 14:44:54 +0200

    Add remaps to nvim

--- nvim/.config/nvim/lua/nvimconfig/remaps.lua
+++ nvim/.config/nvim/lua/nvimconfig/remaps.lua
@@ -1,18 +1,34 @@
 local NORMAL_KEYMAP = {
-
+    {"J", "mzJ`z"},
 }
 
 local INSERT_KEYMAP = {
-    {"jk", "<Esc>"},
+    {"jk",      "<Esc>"},
+    {"<C-H>",   "<Esc>ldbi"}, -- Delete previous word
+    {"<C-Del>", "<Esc>ldei"}, -- Delete next word
 }
 
 local VISUAL_KEYMAP = {
-
+    {"J", ":m '>+1<CR>gv=gv", desc="Move line up"},
+    {"K", ":m '>-2<CR>gv=gv", desc="Move line down"},
 }
 
 local BASE_WHICH_KEYMAP = {
-    { "<leader>q", "<cmd>q<cr>",  desc="Close buffer", icon="󰅗" },
-    { "<leader>Q", "<cmd>qa<cr>", desc="Close all",    icon={ icon="󰅗", color="red" } }
+    { "<leader>q", "<cmd>q<cr>",
+        desc="Close buffer",
+        icon="󰅗"
+    },
+    { "<leader>Q", "<cmd>qa<cr>",
+        desc="Close all",
+        icon={ icon="󰅗", color="red" }
+    },
+    { "<leader>w", "<cmd>set wrap!<CR>",
+        desc="Toggle line wrap",
+        icon={ icon="󰖶", color="orange" }
+    },
+    { "<leader>n", ":set relativenumber!<CR>",
+        desc="Toggle relative numbering"
+    }
 }
 
 for i,m in ipairs(NORMAL_KEYMAP) do
