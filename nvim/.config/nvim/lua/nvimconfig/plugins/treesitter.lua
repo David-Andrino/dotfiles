b660e9896828cb4afa6562ade587598e2f1ba061
David Andrino <davidandrino2002@gmail.com>
2025-05-06 18:25:46 +0200

    Fix nvim docs treesitter error

--- nvim/.config/nvim/lua/nvimconfig/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/nvimconfig/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "cpp", "lua", "python", "rust" },
+            ensure_installed = { "c", "cpp", "lua", "python", "rust", "vimdoc", "luadoc", "vim", "markdown" },
             highlight = { enable = true },
             indent = { enable = true }
         })
