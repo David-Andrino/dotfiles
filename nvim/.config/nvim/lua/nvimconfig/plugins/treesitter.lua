46dc8b6d5be92cadf87ccf7e29e3c1e4200a6ace
David Andrino <david.andrino@upm.es>
2026-08-10 17:39:43 +0200

    nvim: Fix treesitter

--- nvim/.config/nvim/lua/nvimconfig/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/nvimconfig/plugins/treesitter.lua
@@ -1,12 +1,18 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "cpp", "lua", "python", "rust", "vimdoc", "luadoc", "vim", "markdown" },
-            highlight = { enable = true },
-            indent = { enable = true }
+        require("nvim-treesitter").install({
+            "c", "cpp", "lua", "python", "rust", "vimdoc", "luadoc", "vim", "markdown"
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "c", "cpp", "lua", "python", "rust", "vimdoc", "luadoc", "vim", "markdown" },
+            callback = function()
+                vim.treesitter.start()
+                vim.opt_local.indentexpr = "v:lua.vim.treesitter.indentexpr()"
+            end,
         })
     end
 }
-
